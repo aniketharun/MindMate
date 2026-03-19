704baf336452ff2f27c6c1fc86fffd9a6bce37aa
aniketharun <aniketharun2@gmail.com>
2026-03-19 17:00:21 +0530

    fix: frontend url

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://mind-mate-three-rho.vercel.app"
+    "https://mind-mate-aniketharuns-projects.vercel.app"
   ],
   credentials: true,
 }));
